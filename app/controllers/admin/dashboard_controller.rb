d476876224a2c19f61dc2542b8bf4be4c6d22792
Sean Oyler <xrysen@gmail.com>
2021-01-06 23:32:46 +0000

    Adds total product count and total category count to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  def index
+    
+  end
+  
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
