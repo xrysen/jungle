cc7c1febabf260f677073750ac3d8ba9942fddf1
Sean Oyler <xrysen@gmail.com>
2021-01-07 21:41:40 +0000

    Updates sessions controller with logic for logging in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    #if user exists and password is entered correctly
+    if user && user.authenticate(params[:password])
+      #Save user id inside the broswer cookie
+      session[:user_id] = user.id
+      redirect_to "/"
+    else
+      # If user login doesn't work
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
