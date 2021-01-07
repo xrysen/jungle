2f1833ff134870c0384b2bc98d8e3a9e500c48b1
Sean Oyler <xrysen@gmail.com>
2021-01-07 18:44:02 +0000

    Lists all categories under admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,15 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new(:name)
+  end
 end
