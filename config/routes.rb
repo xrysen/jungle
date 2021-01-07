465bb5a7dfbe271606d649e68f17cfa24a5b874b
Sean Oyler <xrysen@gmail.com>
2021-01-07 21:25:05 +0000

    Adds routes for user signup

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
   end
 
   get "/:page" => "static#show"
+
+  get '/signup' => 'user#new'
+  post '/users' => 'users#create'
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
