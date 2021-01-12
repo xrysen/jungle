6e1d738018de7e8a9c235befd71a44bf6355c813
Sean Oyler <xrysen@gmail.com>
2021-01-12 03:41:33 +0000

    Removes comments from SessionController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,6 @@ class SessionsController < ApplicationController
   def new
   end
 
-  # def create
-  #   puts User.myTest
-  #   user = User.find_by_email(params[:email])
-  #   #if user exists and password is entered correctly
-  #   if user && user.authenticate(params[:password])
-  #     #Save user id inside the broswer cookie
-  #     session[:user_id] = user.id
-  #     redirect_to "/"
-  #   else
-  #     # If user login doesn't work
-  #     redirect_to '/login'
-  #   end
-  # end
-
   def create
     if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
